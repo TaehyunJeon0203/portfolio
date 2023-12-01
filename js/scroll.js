358478ab3d06b73cee3d1f494b47978aec885409
전태현 <jeontaehyeon@jeontaehyeons-MacBook-Air.local>
2023-12-01 17:57:25 +0900

    스크롤 안되는 오류 수정 필요

---
+++ js/scroll.js
@@ -0,0 +1,32 @@
+window.addEventListener(`wheel`, function(e) {
+    e.preventDefault();
+}, { passive: false });
+
+let html = document.querySelector("html");
+let page = 1;
+let lastPage = document.querySelectorAll(".content").length;
+
+html.scrollTo({ top: 0, behavior: 'smooth' });
+
+window.addEventListener(`wheel`, function(e) {
+    // 만약 애니메이션이 진행 중이라면 리턴
+    if (isAnimated(html)) return;
+
+    // 마우스 휠 방향에 따라 페이지 이동
+    if (e.deltaY > 0) {
+        if (page == lastPage) return;
+        page++;
+    } else if (e.deltaY < 0) {
+        if (page == 1) return;
+        page--;
+    }
+
+    // 새로운 페이지의 위치 계산 및 애니메이션
+    let posTop = (page -1) * this.window.innerHeight;
+    html.scrollTo({ top: posTop, behavior: 'smooth' });
+});
+
+// 애니메이션이 진행 중인지 확인하는 함수
+function isAnimated(element) {
+    return getComputedStyle(element).getPropertyValue('scroll-behavior')
+};
